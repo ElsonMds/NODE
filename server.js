33e3598f7fd9e061e7506853daf99884a002f105
ElsonMds <elsontechnologies2020@gmail.com>
2020-07-31 19:25:00 -0300

    todas as rotas implementadas no modulo do servidor

--- server.js
+++ server.js
@@ -13,9 +13,23 @@ const app= express();
 
 
 //rota - manipulador de rota
-app.get('/',(req,res)=>{
-    res.send('Olá mundo!')
+app.get('/api/v1/bootcamps',(req,res)=>{
+    res.status(200).json({sucesso:true,msg:'mostrando todos os bootcamps'})
 })
+
+app.post('/api/v1/bootcamps',(req,res)=>{
+    res.status(200).json({sucesso:true,msg:'criar novo bootcamps'})
+})
+
+app.put('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({sucesso:true,msg:`atualizar bootcamp ${req.params.id}`})
+})
+
+
+app.delete('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({sucesso:true,msg:`atualizar bootcamp ${req.params.id}`})
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Servidor rodando no modo ${process.env.NODE_ENV} na porta ${PORT}`);
