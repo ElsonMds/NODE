cc6d45cf0b9c9dcdba313f9731307024e610d191
ElsonMds <elsontechnologies2020@gmail.com>
2020-08-01 10:54:29 -0300

    implementei um middleware e instalei o third-party  morgan

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bootcapms=require('./routes/bootcamps');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const bootcapms = require("./routes/bootcamps");
+const logger    = require('./middlewares/logger');
 ///trazer as configuraçoes dotenv
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
+const morgan= require('morgan');
+
 
 //inicializa o app com express
-const app= express();
+const app = express();
 
 
 
+if(process.env.NODE_ENV==='development'){
+    //console.log('++++++++++++++++++++++++++++')
+    app.use(morgan('dev'));
+}
 
 
 //para toda requisicao ao ponto '/api/v1/bootcamps' delegue ao modulo bootcamps
-app.use('/api/v1/bootcamps',bootcapms);
+app.use("/api/v1/bootcamps", bootcapms);
 
-app.listen(PORT,()=>{
-    console.log(`Servidor rodando no modo ${process.env.NODE_ENV} na porta ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(
+    `Servidor rodando no modo ${process.env.NODE_ENV} na porta ${PORT}`
+  );
+});
